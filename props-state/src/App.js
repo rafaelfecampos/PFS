7ed63be9b273e52bda17a6558bc1b76a5224f044
Rafael <2001campos.rafael@gmail.com>
2024-08-20 21:52:09 -0300

    aula 20/08

--- props-state/src/App.js
+++ props-state/src/App.js
@@ -1,9 +1,107 @@
-import './App.css';
-import { faker } from '@faker-js/faker';
-import React from "react";
-import Evento from './components/Evento';
-import Spinner from './components/Spinner';
+import React, { useState, useEffect } from 'react';
 
+
+const MenorQue10 = () => {
+  useEffect(() => {
+    console.log(`Iniciado`);
+
+    return (
+      () => console.log(`Finalizado`)
+    );
+  }, []);
+
+  return <p>menor que 10...</p>;
+}
+
+const App = () => {
+  const [cliques, setCliques] = useState(0);
+
+  const menor10 = cliques < 10 ? <MenorQue10 /> : null;
+
+  return (
+    <div>
+      <h4>Clicou {cliques} vezes.</h4>
+      {menor10}
+      <button onClick={() => { setCliques(cliques + 1) }}>Clique</button>
+    </div>
+  );
+};
+
+export default App;
+
+
+
+
+// import './App.css';
+// import { faker } from '@faker-js/faker';
+// import React, {useState, useEffect} from "react";
+// import Evento from './components/Evento';
+// import Spinner from './components/Spinner';
+
+// const App = (props) => {
+//   //Hooks
+//   const [dados, setDados] = useState([]);
+//   const [carregando, setCarregando] = useState(true);
+  
+//   useEffect(() => {
+//     setTimeout(() => {
+//       console.log(`Carregando: ${carregando}`);
+//       setCarregando(false);
+//       setDados(objetos);
+//     }, 2000);
+//   },[]);
+  
+//   if (carregando) {
+//     return <Spinner />;
+//   }
+
+//   return (<div className="ui feed">
+//       {
+//         dados.map(x => {
+//           return (<Evento 
+//             nome={x.nome} 
+//             likes={x.likes} 
+//             mensagem={x.mensagem} 
+//             fotoPerfil={x.fotoPerfil} 
+//             tempo={x.tempo} 
+//           />);
+//         })
+//       }
+//     </div>);
+// };
+
+// const objetos = [
+//   {
+//     nome: faker.person.firstName(),
+//     likes: faker.number.int({min: 0, max: 50}),
+//     mensagem: faker.lorem.sentence(),
+//     fotoPerfil: faker.image.avatar(),
+//     tempo: faker.number.int({min: 0, max: 50})
+//   },
+//   {
+//     nome: faker.person.firstName(),
+//     likes: faker.number.int({min: 0, max: 50}),
+//     mensagem: faker.lorem.sentence(),
+//     fotoPerfil: faker.image.avatar(),
+//     tempo: faker.number.int({min: 0, max: 50})
+//   },
+//   {
+//     nome: faker.person.firstName(),
+//     likes: faker.number.int({min: 0, max: 50}),
+//     mensagem: faker.lorem.sentence(),
+//     fotoPerfil: faker.image.avatar(),
+//     tempo: faker.number.int({min: 0, max: 50})
+//   },
+//   {
+//     nome: faker.person.firstName(),
+//     likes: faker.number.int({min: 0, max: 50}),
+//     mensagem: faker.lorem.sentence(),
+//     fotoPerfil: faker.image.avatar(),
+//     tempo: faker.number.int({min: 0, max: 50})
+//   },
+// ]
+// export default App;
+//------------------------------------------------------------------------
 // function App() {
 //   return (
 //     <div className='ui feed'>
@@ -16,66 +114,34 @@ import Spinner from './components/Spinner';
 //   );
 // }
 
-class App extends React.Component{
-  constructor(props){
-    super(props);
-    this.state = {feeds:[], carregado: false};
-  }
+// class App extends React.Component{
+//   constructor(props){
+//     super(props);
+//     this.state = {feeds:[], carregado: false};
+//   }
 
-  render(){
-    if(!this.state.carregado){
-      return (
-      <Spinner /*mensagem="Está carregando"*//> //conseguimos deixar uma variável declarada preenchida e substituir seu valor ao utilizar o componente em que ela pertence
-      //observe o defaultProps no componente Spinner, tem-se defaultProps definindo o valor de mensagem
-      )
-    }
-    return (
-      <div className='ui feed'>
-        {
-          this.state.feeds.map( x => {
-            return <Evento nome={x.nome} likes={x.likes} mensagem={x.mensagem} fotoPerfil={x.fotoPerfil} tempo={x.tempo} />
-          })
-        }
-      </div>
-    );
-  }
+//   render(){
+//     if(!this.state.carregado){
+//       return (
+//       <Spinner/> /*mensagem="Está carregando"*/ //conseguimos deixar uma variável declarada preenchida e substituir seu valor ao utilizar o componente em que ela pertence
+//       //observe o defaultProps no componente Spinner, tem-se defaultProps definindo o valor de mensagem
+//       )
+//     }
+//     return (
+//       <div className='ui feed'>
+//         {
+//           this.state.feeds.map( x => {
+//             return <Evento nome={x.nome} likes={x.likes} mensagem={x.mensagem} fotoPerfil={x.fotoPerfil} tempo={x.tempo} />
+//           })
+//         }
+//       </div>
+//     );
+//   }
   
-  componentDidMount() {
-    setTimeout(() => {
-      this.setState({ feeds: objetos, carregado: true});
+//   componentDidMount() {
+//     setTimeout(() => {
+//       this.setState({ feeds: objetos, carregado: true});
       
-    },1250)
-  }
-}
-
-const objetos = [
-  {
-    nome: faker.person.firstName(),
-    likes: faker.number.int({min: 0, max: 50}),
-    mensagem: faker.lorem.sentence(),
-    fotoPerfil: faker.image.avatar(),
-    tempo: faker.number.int({min: 0, max: 50})
-  },
-  {
-    nome: faker.person.firstName(),
-    likes: faker.number.int({min: 0, max: 50}),
-    mensagem: faker.lorem.sentence(),
-    fotoPerfil: faker.image.avatar(),
-    tempo: faker.number.int({min: 0, max: 50})
-  },
-  {
-    nome: faker.person.firstName(),
-    likes: faker.number.int({min: 0, max: 50}),
-    mensagem: faker.lorem.sentence(),
-    fotoPerfil: faker.image.avatar(),
-    tempo: faker.number.int({min: 0, max: 50})
-  },
-  {
-    nome: faker.person.firstName(),
-    likes: faker.number.int({min: 0, max: 50}),
-    mensagem: faker.lorem.sentence(),
-    fotoPerfil: faker.image.avatar(),
-    tempo: faker.number.int({min: 0, max: 50})
-  },
-]
-export default App;
+//     },1250)
+//   }
+// }
--- props-state/src/components/Evento.js
+++ props-state/src/components/Evento.js
@@ -2,37 +2,62 @@ import React from "react";
 import { faker } from '@faker-js/faker';
 
 
-class Evento extends React.Component{
-  constructor(props){
-    super(props);
-    }
+// class Evento extends React.Component{
+//   constructor(props){
+//     super(props);
+//     }
 
-    render(){
-      return(
-          <div className="ui feed">
-        <div className="event">
+//     render(){
+//       return(
+//           <div className="ui feed">
+//         <div className="event">
+//           <div className="label">
+//             <img src={this.props.fotoPerfil}/>
+//           </div>
+//           <div className="content">
+//             <div className="summary">
+//               <a className="user">
+//                 {this.props.nome}
+//               </a> {this.props.mensagem}
+//               <div className="date">
+//                 {this.props.tempo} minutos 
+//               </div>
+//             </div>
+//             <div className="meta">
+//               <a className="like">
+//                 <i className="like icon"></i> {this.props.likes} Likes
+//               </a>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//       )
+//     }
+// };
+
+const Evento = (props) => {
+  return (
+      <div className="event">
           <div className="label">
-            <img src={this.props.fotoPerfil}/>
+              <img src={props.fotoPerfil} alt="..." />
           </div>
           <div className="content">
-            <div className="summary">
-              <a className="user">
-                {this.props.nome}
-              </a> {this.props.mensagem}
-              <div className="date">
-                {this.props.tempo} minutos 
+              <div className="summary">
+                  <a className="user">
+                      {props.nome}
+                  </a> {props.mensagem}
+                  <div className="date">
+                      {props.tempo} minutos atrás
+                  </div>
               </div>
-            </div>
-            <div className="meta">
-              <a className="like">
-                <i className="like icon"></i> {this.props.likes} Likes
-              </a>
-            </div>
+              <div className="meta">
+                  <a className="like">
+                      <i className="like icon"></i> {props.likes} Likes
+                  </a>
+              </div>
           </div>
-        </div>
       </div>
-      )
-    }
+  );
 };
 
 
