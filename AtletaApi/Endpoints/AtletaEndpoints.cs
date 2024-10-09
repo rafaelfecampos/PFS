b124999b8feff541c06944ebd112f9d3c1893473
Rafael <2001campos.rafael@gmail.com>
2024-10-08 21:47:22 -0300

    aula 08/10

---
+++ AtletaApi/Endpoints/AtletaEndpoints.cs
@@ -0,0 +1,11 @@
+﻿
+namespace AtletaApi;
+
+public static class AtletaEndpoints
+{
+    //internal x public => o internal limita o acesso da função à dll
+    internal static void AdicionarAtletaEndpoints(this WebApplication app)
+    {
+        throw new NotImplementedException();
+    }
+}
